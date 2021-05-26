5c528a7af9e24601fa6f13f2ffc8a06bf98f2bbc
HARSHITPRATAP7121 <83752601+HARSHITPRATAP7121@users.noreply.github.com>
2021-05-26 23:28:56 +0530

    Add files via upload

---
+++ use of variable.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ int main()
+  { 
+      int var1 ,var2 ;
+      var1=15;
+      var2=45;
+      printf(" printing of variable var1:%d \n ", var1);
+      printf(" printing of variable var2 :%d " , var2 );
+      return 0 ;
+  }
