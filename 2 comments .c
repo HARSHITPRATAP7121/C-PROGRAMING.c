2ff527ed16754a741777c89b4b4be33237f0540a
HARSHITPRATAP7121 <83752601+HARSHITPRATAP7121@users.noreply.github.com>
2021-05-26 23:28:18 +0530

    Add files via upload

---
+++ 2 comments .c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ()
+{  
+    // i am adding a single line comment
+    
+/*this is  a double line comment 
+ and i print my name here */
+  printf (" my name is harshit pratap");
+  return 0 ;
+}
