ac394cd086eaa54ea04d9d958272d9b2f9ac26c0
HARSHITPRATAP7121 <83752601+HARSHITPRATAP7121@users.noreply.github.com>
2021-05-27 23:03:56 +0530

    Add files via upload

---
+++ 12 increment or decrement.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a = 15, b = 20;
+  printf(" printing increment and decrement \n");
+    printf("++a = %d \n", ++a);
+    printf("--a = %d \n", --a);
+    printf("++b = %d \n", ++b);
+    printf("--b = %d \n", --b);
+    return 0;
+}
