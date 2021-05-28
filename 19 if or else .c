4e0843ccc7a9028bb6f442cbc8478deadeb33cae
HARSHITPRATAP7121 <83752601+HARSHITPRATAP7121@users.noreply.github.com>
2021-05-28 16:10:52 +0530

    Add files via upload

---
+++ 19 if or else .c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int  a ,b;
+    a=10;
+    b > a ;
+    printf("  printing my name put number greater than ten : %d \n " , b>a); 
+    scanf("%d", &b);
+    if (b>a)
+    {
+       printf("my name is  harshit pratap");
+    }
+    else
+    {
+        printf(" you put wrong number");
+        
+    }
+    return 0;
+}
