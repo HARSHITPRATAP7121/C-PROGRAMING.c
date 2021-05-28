2bdc16e15e6b1cbff95356dbdeaf3e0633f69eb0
HARSHITPRATAP7121 <83752601+HARSHITPRATAP7121@users.noreply.github.com>
2021-05-28 16:10:28 +0530

    Add files via upload

---
+++ 18 if example -.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int  a ,b;
+    a=1;
+    b > a ;
+    printf(" for  printing my name put number greater than one : %d \n " , b>a); 
+    scanf("%d", &b);
+    if (b>a)
+    {
+        printf("my name is  harshit pratap");
+    }
+    return 0;
+}
